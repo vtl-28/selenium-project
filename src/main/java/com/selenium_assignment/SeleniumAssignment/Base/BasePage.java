d6de2e18c371985000e6978a9ced464620d70117
Vuisile Lehola <vuisile.lehola@nagarro.com>
2025-08-15 08:18:52 +0200

    add a wait until element is presence condition before finding and returning the element

--- src/main/java/com/selenium_assignment/SeleniumAssignment/Base/BasePage.java
+++ src/main/java/com/selenium_assignment/SeleniumAssignment/Base/BasePage.java
@@ -1,9 +1,14 @@
 package com.selenium_assignment.SeleniumAssignment.Base;
 
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 	public static WebDriver driver;
@@ -14,7 +19,10 @@ public class BasePage {
 	}
 
 	protected static WebElement find(By locator) {
-		return driver.findElement(locator);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        return driver.findElement(locator);
 	}
 
 	protected void set(By locator, String text) {
